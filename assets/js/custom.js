fb9f94807a5cb65fce79ea6cfe20a9509f646812
Susan Price <susaneprice@gmail.com>
2017-02-05 15:18:37 -0800

    toggle visibility of search input field

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,3 +6,10 @@
  */
 
 // window.alert('hello');
+
+function showSearchInput() {
+    document.getElementById("search-entry").innerHTML = "in showSearchInput()";
+    document.getElementById("search-entry").style.visibility = "visible";
+    document.getElementById("main-navbar-collapse-1").style.visibility = "hidden";
+
+}
