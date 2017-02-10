2bfd1407cb9ea1bd0613e09c17b9384226e9eb23
Susan Price <susaneprice@gmail.com>
2017-02-10 14:31:26 -0800

    added sidebar and container & row classes

--- single.php
+++ single.php
@@ -9,27 +9,36 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<!-- BLOG CONTENT
+    ================================================== -->
+    <div class="container">
+        <div id="primary" class="row" >
+            <main id="content" class="col-sm-8">		
 
-		<?php
-		while ( have_posts() ) : the_post();
+			<?php
+			while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+				get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+				the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #content -->
 
-<?php
-get_sidebar();
-get_footer();
+			<!-- SIDEBAR
+    		================================================== -->
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+
+		</div><!-- #primary -->
+	</div><!-- #container -->
+
+<? get_footer(); ?>
