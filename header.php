9f8ba1d08542e1b372b9d37583469481e966fc7d
Susan Price <susaneprice@gmail.com>
2017-02-09 16:44:17 -0800

    removed loading of featured image in header

--- header.php
+++ header.php
@@ -28,73 +28,52 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'idealist' ); ?></a>
 
+	<!-- HEADER
+    ================================================== -->
+    <header id="masthead" class="site-header">
+        <div class="navbar-wrapper">
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
 
- 		<!-- HEADER
-        ================================================== -->
-        <header id="masthead" class="site-header">
-            <div class="navbar-wrapper">
-                <nav class="navbar navbar-inverse">
-                    <div class="container-fluid">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar-collapse-1" aria-expanded="false">
-                                <span class="sr-only">Toggle navigation</span>
-                                <!-- mobile menu -->
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="/">IDEALIST</a>
-                            <input type="text" id="search-entry" class="form-control" placeholder="search">
-                        </div>
-
-						<div class="collapse navbar-collapse" id="main-navbar-collapse-1">
-						    <ul class="nav navbar-nav">
-								<!-- If the menu location is not set in the Admin, 
-								then by default all pages will appear in menu  -->
-								 <?php
-						            wp_nav_menu( array(
-						                'menu'              => 'primary',
-						                'theme_location'    => 'primary',
-						                'depth'             => 2,
-						                'container'         => 'div',
-						                'container_class'   => 'collapse navbar-collapse',
-						        		'container_id'      => 'bs-example-navbar-collapse-1',
-						                'menu_class'        => 'nav navbar-nav',
-						                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-						                'walker'            => new wp_bootstrap_navwalker())
-						            );
-						        ?>
-  							</ul>
-			      
-                            <div class="nav navbar-nav navbar-right">
-                                <button type="button" class="btn btn-default" onclick="showSearchInput()">
-                                    <i class="fa fa-search" aria-hidden="true"></i>
-                                </button>
-                            </div>
-					    </div><!-- /.navbar-collapse -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <!-- mobile menu -->
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="/">IDEALIST</a>
+                        <input type="text" id="search-entry" class="form-control" placeholder="search">
                     </div>
-                </nav>
-            </div>
-        </header>
 
-
-
-
-
-
-
-
-
-
-	<?php
-	// If a regular post or page, and not the front page, show the featured image.
-	if ( has_post_thumbnail() && ( is_single() || ( is_page() && !is_frontpage() ) ) ) :
-		echo '<div class="single-featured-image-header">';
-		the_post_thumbnail();
-		echo '</div><!-- .single-featured-image-header -->';
-	endif;
-	?>
-
-	<div id="content" class="site-content">
+					<div class="collapse navbar-collapse" id="main-navbar-collapse-1">
+					    <ul class="nav navbar-nav">
+							<!-- If the menu location is not set in the Admin, 
+							then by default all pages will appear in menu  -->
+							 <?php
+					            wp_nav_menu( array(
+					                'menu'              => 'primary',
+					                'theme_location'    => 'primary',
+					                'depth'             => 2,
+					                'container'         => 'div',
+					                'container_class'   => 'collapse navbar-collapse',
+					        		'container_id'      => 'bs-example-navbar-collapse-1',
+					                'menu_class'        => 'nav navbar-nav',
+					                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+					                'walker'            => new wp_bootstrap_navwalker())
+					            );
+					        ?>
+							</ul>
+		      
+                        <div class="nav navbar-nav navbar-right">
+                            <button type="button" class="btn btn-default" onclick="showSearchInput()">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </button>
+                        </div>
+				    </div><!-- /.navbar-collapse -->
+                </div>
+            </nav>
+        </div>
+    </header>
