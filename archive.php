1f575d915bde046976cca6d3bf8facf39a56553e
Susan Price <susaneprice@gmail.com>
2017-02-13 10:38:46 -0800

    archive layout now consistent with other pages, using 8+4 grid and displays featured image first

--- archive.php
+++ archive.php
@@ -9,43 +9,50 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
 		<?php
 		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			<section class="feature-image feature-image-default-alt img-responive" data-type="background" data-speed="2">
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
+			</section>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+			<div class="container">
+				<div id="primary" class="row">
+					<main id="content" class="col-sm-8">
 
-			endwhile;
+						<?php
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
 
-			the_posts_navigation();
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_format() );
 
-		else :
+						endwhile;
 
-			get_template_part( 'template-parts/content', 'none' );
+						the_posts_navigation();
 
-		endif; ?>
+						else :
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+						get_template_part( 'template-parts/content', 'none' );
 
+						endif; ?>
+
+					</main><!-- #main -->
+
+					<!-- SIDEBAR
+		    		================================================== -->
+					<aside class="col-sm-4">
+					<?php get_sidebar(); ?>
+					</aside>
+
+				</div --><!-- #primary -->
+			</div><!-- #container -->
 <?php
-get_sidebar();
 get_footer();
