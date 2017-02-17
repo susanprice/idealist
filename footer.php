19bf9de29fdf5e372224f5bf2677e8a1de1b08d1
Susan Price <susaneprice@gmail.com>
2017-02-17 11:22:55 -0800

    simplified to display centered copyright only

--- footer.php
+++ footer.php
@@ -16,28 +16,10 @@
 <div class="navbar-wrapper">
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
-            <div class="col-sm-6 col-md-3 footer-nav">
-                <!-- button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                </button -->
-                <a class="navbar-brand" href="/">IDEALIST</a>
-            </div>
-            <div class="hidden-sm hidden-xs col-md-6">
-                <nav class="center">
-                    <ul class="nav navbar-nav">
-                        <li><a href="/">Home<span class="sr-only">(current)</span></a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/contact">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="col-sm-6 col-md-3">
-                <div class="vcenter">
+            <div class="col-md-12">
+                <nav class="center vcenter">
                     <p class="copyright">Copyright&copy;2017</p>
-                </div>
+                </nav>
             </div>
         </div>
     </nav>
