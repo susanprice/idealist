f83436c42ebd2a61b9d9882b60073d1565eff80f
Susan Price <susaneprice@gmail.com>
2017-02-18 14:12:39 -0800

    featured image (or default) now loaded in header. updated comments

--- index.php
+++ index.php
@@ -2,21 +2,15 @@
 /**
  * The main template file
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package idealist
+ * @package WordPress
+ * @subpackage Idealist
+ * @since 1.0
+ * @version 1.1
  */
 
 get_header(); ?>
-
-	<section class="feature-image feature-image-default-alt img-responive" data-type="background" data-speed="2">
-		<!-- h1 class="page-title">Blog</h1 -->
-	</section>
 
 	<!-- BLOG CONTENT
 	================================================== -->
