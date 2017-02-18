2a0327630c5f2a7a8956ca2d7570fb67514d9246
Susan Price <susaneprice@gmail.com>
2017-02-18 14:19:58 -0800

    removed featured image loading (now in header) and added article tag for panel

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -1,41 +1,34 @@
 <?php
-/* Template Name: Full-width Template */
+/** 
+ * Template Name: Full-width Template 
+ *
+ * For pages and posts using full width
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Idealist
+ * @since 1.0
+ * @version 1.1
+*/
 
-$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
-get_header();
-?>
-
-<!-- FEATURE IMAGE
-================================================== -->
-
-<?php if (has_post_thumbnail() ) { ?>
-
-<section class="feature-image feature-image-default img-responive" style="background:url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: cover;" data-type="background" data-speed="2">
-    <h1 class="page-title"><?php the_title(); ?></h1>
-</section>
-
-<?php } else { // Fallback image ?>
-
-<section class="feature-image feature-image-default img-responive" data-type="background" data-speed="2">
-    <h1 class="page-title"><?php the_title(); ?></h1>
-</section>
-
-<?php } ?>
-
+get_header(); ?>
 
 <!-- MAIN CONTENT
 ================================================== -->
 <div class="container">
-
     <div class="row" id="primary">
         <div id="content" class="col-md-12">
-            <section class="main-content">
-                <?php while ( have_posts() ) : the_post(); ?>
-
-                    <?php the_content(); ?>
-
-                <?php endwhile; // end of the loop ?>    
-            </section>
+            <article class="post">
+                <header>
+                    <h2 class="page-title"><?php the_title(); ?></h2>
+                </header>
+                <section class="main-content">
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php the_content(); ?>
+                    <?php endwhile; // end of the loop ?>    
+                </section>
+            </article>    
         </div>
     </div>
 </div>
