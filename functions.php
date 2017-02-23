20c3473d0e1319bec8f63f255fb830b0a5cf9b5a
Susan Price <susaneprice@gmail.com>
2017-02-23 07:25:03 -0800

    calling all add_theme_support functions together and cleaning up comments

--- functions.php
+++ functions.php
@@ -16,51 +16,21 @@ if ( ! function_exists( 'idealist_setup' ) ) :
  * as indicating support for post thumbnails.
  */
 function idealist_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on idealist, use a find and replace
-	 * to change 'idealist' to the name of your theme in all the template files.
-	 */
+
+	// Make theme available for translation.
 	load_theme_textdomain( 'idealist', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
+	// Let WordPress manage the document title.
 	add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
+	// Enable support for Post Thumbnails on posts and pages.
 	add_theme_support( 'post-thumbnails' );
 
-	add_editor_style( array( 'assets/css/editor-style.css', idealist_fonts_url() ) );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-    	'primary' => __( 'Primary Menu', 'idealist' ),
-    	// 'social' => __( 'Social Links Menu', 'idealist' ),
-	) );
-	
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+	// Add HTML5 support
+	add_theme_support( 'html5', array('search-form', 'comment-form', 'comment-list', 'caption',) );
 
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'idealist_custom_background_args', array(
@@ -70,6 +40,15 @@ function idealist_setup() {
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	add_editor_style( array( 'assets/css/editor-style.css', idealist_fonts_url() ) );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+    	'primary' => __( 'Primary Menu', 'idealist' ),
+    	// 'social' => __( 'Social Links Menu', 'idealist' ),
+	) );
+	
 }
 endif;
 add_action( 'after_setup_theme', 'idealist_setup' );
@@ -189,9 +168,8 @@ function idealist_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'idealist_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
+
+// Add "Header Image" option in the Customizer
 require get_template_directory() . '/inc/custom-header.php';
 
 /**
