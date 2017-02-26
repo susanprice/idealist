6650fe5d5f7e0a308f3163f1786204506ee59152
Susan Price <susaneprice@gmail.com>
2017-02-25 20:20:49 -0800

    added nav menu for social icons (twitter) in footer. centered twitter icon and copyright, and put on separate lines

--- footer.php
+++ footer.php
@@ -14,12 +14,24 @@
 <!-- Footer
 ================================================== -->
 <div class="navbar-wrapper">
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar">
         <div class="container-fluid">
-            <div class="col-md-12">
-                <nav class="center vcenter">
-                    <p class="copyright">Copyright &copy; 2017</p>
+
+           <div class="col-sm-12 col-md-12 col-lg-12">
+                <nav>
+                  <?php wp_nav_menu( array( 
+                        'menu'              => 'social',
+                        'theme_location'    => 'social',
+                        'container_class'   => 'social_menu_class',
+                        'link_before'       => '<span class="screen-reader-text">',
+                        'link_after'        => '</span>',
+                        'fallback_cb'       => false
+                         ) ); ?>  
                 </nav>
+            </div>    
+
+           <div class="col-sm-12 col-md-12 col-lg-12">
+                <p class="copyright">Copyright &copy; 2017</p>
             </div>
         </div>
     </nav>
