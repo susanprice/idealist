5f942049cf983414a601cf97a92abc27192163ef
Susan Price <susaneprice@gmail.com>
2017-03-28 20:24:30 -0700

    customizer color section - added settings & controls for content and link colors

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,63 +11,38 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function idealist_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-
-    /* add a setting and control to an existing section (color) */
-    /* TODO replace date picker with color picker */
-    $wp_customize->add_setting( 'setting_id', array(
-        'type'                    => 'theme_mod', 
-        'capability'              => 'edit_theme_options',
-        'theme_supports'          => '', 
-        'default'                 => '',
-        'transport'               => 'refresh', 
-        'sanitize_callback'       => '',
-        'sanitize_js_callback'    => '', 
-    ) );
-
-    $wp_customize->add_control( 'setting_id', array(
-        'type'              => 'date',
-        'priority'          => 10, 
-        'section'           => 'colors', 
-        'label'             => __( 'Date' ),
-        'description'       => __( 'This is a date control with a red border.' ),
-        'input_attrs'       => array(
-            'class'         => 'my-custom-class-for-js',
-            'style'         => 'border: 1px solid #900',
-            'placeholder'   => __( 'mm/dd/yyyy' ),
-            ),
-        'active_callback' => 'is_front_page',
-    ) );
-
-    /* add a setting and control to a new section (footer) */
-    /* TODO replace date picker with a custom string or copyright notice */
-    $wp_customize->add_section( 'footer' , array(
-        'title'      => __( 'Footer', 'idealist' ),
-        'priority'   => 30,
-    ) );
-
-    $wp_customize->add_setting( 'copyright_id', array(
-        'type'                    => 'theme_mod', 
-        'capability'              => 'edit_theme_options',
-        'theme_supports'          => '', 
-        'default'                 => '',
-        'transport'               => 'refresh', 
-        'sanitize_callback'       => '',
-        'sanitize_js_callback'    => '', 
-    ) );
-
-    $wp_customize->add_control( 'copyright_id', array(
-          'label' => __( 'Copyright Notice' ),
-          'type' => 'textarea',
-          'section' => 'footer',
-        ) );
-
-
-    // Hide core sections/controls when they aren't used on the current page.
-    $wp_customize->get_section( 'header_image' )->active_callback = 'is_front_page';
-    $wp_customize->get_control( 'blogdescription' )->active_callback = 'is_front_page';
+	$colors = array();
+    $colors[] = array(
+      'slug'=>'content_text_color', 
+      'default' => '#333',
+      'label' => __('Content Text Color', 'idealist')
+    );
+    $colors[] = array(
+      'slug'=>'content_link_color', 
+      'default' => '#88C34B',
+      'label' => __('Content Link Color', 'idealist')
+    );
+    foreach( $colors as $color ) {
+      // SETTINGS
+      $wp_customize->add_setting(
+        $color['slug'], array(
+          'default' => $color['default'],
+          'type' => 'option', 
+          'capability' => 
+          'edit_theme_options'
+        )
+      );
+      // CONTROLS
+      $wp_customize->add_control(
+        new WP_Customize_Color_Control(
+          $wp_customize,
+          $color['slug'], 
+          array('label' => $color['label'], 
+          'section' => 'colors',
+          'settings' => $color['slug'])
+        )
+      );
+    }
 }
 add_action( 'customize_register', 'idealist_customize_register' );
 
