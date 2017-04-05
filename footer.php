3b2abe981e7d45dd5661f2e200ab3945bee72adf
Susan Price <susaneprice@gmail.com>
2017-04-04 17:38:35 -0700

    version 1.0.0 submitted. removed copyright and social menu

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package idealist
+ * @package Idealist
  */
 
 ?>
@@ -17,21 +17,21 @@
     <nav class="navbar">
         <div class="container-fluid">
         <hr>
-           <div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="col-sm-12 col-md-12 col-lg-12">
                 <nav>
-                  <?php wp_nav_menu( array( 
+                    <!-- ?php wp_nav_menu( array( 
                         'menu'              => 'social',
                         'theme_location'    => 'social',
                         'container_class'   => 'social_menu_class',
                         'link_before'       => '<span class="screen-reader-text">',
                         'link_after'        => '</span>',
                         'fallback_cb'       => false
-                         ) ); ?>  
+                         ) ); ? -->  
                 </nav>
             </div>    
 
             <div class="col-sm-12 col-md-12 col-lg-12">
-                <p class="copyright">Copyright &copy; 2017</p>
+                <!-- p class="copyright">Copyright &copy; 2017</p -->
             </div>
         </div>
     </nav>
