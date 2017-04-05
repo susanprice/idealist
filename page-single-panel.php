a0b43d3ba730ab478944cdf761a0fe968b42c98f
Susan Price <susaneprice@gmail.com>
2017-04-04 17:40:50 -0700

    version 1.0.0 submitted

--- page-single-panel.php
+++ page-single-panel.php
@@ -1,35 +1,36 @@
 <?php
-/* 
- * Template Name: Single Panel Template 
- *
- * For placing content inside a bordered panel for readability
+/**
+ * Template Name: Single Panel Template
+ *   
+ * For placing content inside panels for readability.
+ * This template displays one panel width-wise
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package WordPress
- * @subpackage Idealist
- * @since 1.0
- * @version 1.1
-*/
+ * @package Idealist
+ */
 
 get_header(); ?>
 
 <!-- MAIN CONTENT
 ================================================== -->
 <div class="container">
-    <div class="row" id="primary">
-        <div class="col-md-2"></div>
-        <div id="content" class="col-md-8 panel">
-            <header>
-                <h2 class="page-title"><?php the_title(); ?></h2>
-            </header>
-            <section class="main-content">
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <?php the_content(); ?>
-                <?php endwhile; // end of the loop ?>    
-            </section>
+    <div class="row">
+        <div class="col-md-12">
+            <article class="post">
+            
+                <header>
+                    <h2 class="page-title"><?php the_title(); ?></h2>
+                </header>
+
+                <section class="main-content">
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <?php the_content(); ?>
+                    <?php endwhile; // end of the loop ?>    
+                </section>
+
+            </article>
         </div>
-        <div class="col-md-2"></div>
     </div>
 </div>
 
