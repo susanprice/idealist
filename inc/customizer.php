02962c1672f387cc23c5e7a58d0c24d805bdf0c6
Susan Price <susaneprice@gmail.com>
2017-04-04 17:43:19 -0700

    version 1.0.0 submitted

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * idealist Theme Customizer
  *
- * @package idealist
+ * Idealist: Customizer
+ *
+ * @package Idealist
+ *
  */
 
 /**
@@ -11,39 +13,39 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function idealist_customize_register( $wp_customize ) {
-	$colors = array();
+    $colors = array();
     $colors[] = array(
-      'slug'=>'content_text_color', 
-      'default' => '#333',
-      'label' => __('Content Text Color', 'idealist')
+        'slug'=>'content_text_color', 
+        'default' => '#333',
+        'label' => __('Content Text Color', 'idealist')
     );
     $colors[] = array(
-      'slug'=>'content_link_color', 
-      'default' => '#88C34B',
-      'label' => __('Content Link Color', 'idealist')
+        'slug'=>'content_link_color', 
+        'default' => '#88C34B',
+        'label' => __('Content Link Color', 'idealist')
     );
     foreach( $colors as $color ) {
-      // SETTINGS
-      $wp_customize->add_setting(
-        $color['slug'], array(
-          'default' => $color['default'],
-          'type' => 'option', 
-          'capability' => 
-          'edit_theme_options'
-        )
-      );
-      // CONTROLS
-      $wp_customize->add_control(
-        new WP_Customize_Color_Control(
-          $wp_customize,
-          $color['slug'], 
-          array('label' => $color['label'], 
-          'section' => 'colors',
-          'settings' => $color['slug'])
-        )
-      );
+        // SETTINGS
+        $wp_customize->add_setting(
+            $color['slug'], array(
+              'default' => $color['default'],
+              'type' => 'option', 
+              'capability' => 'edit_theme_options',
+              'sanitize_callback' => 'sanitize_hex_color',
+            )
+        );
+        // CONTROLS
+        $wp_customize->add_control(
+            new WP_Customize_Color_Control(
+                $wp_customize,
+                $color['slug'], 
+                array('label' => $color['label'], 
+                    'section' => 'colors',
+                    'settings' => $color['slug'])
+                )
+            );
+        }
     }
-}
 add_action( 'customize_register', 'idealist_customize_register' );
 
 
