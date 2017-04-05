34fe43486c200bc97f627a6adb911d4e8f1072f8
Susan Price <susaneprice@gmail.com>
2017-04-04 17:39:58 -0700

    version 1.0.0 submitted

--- page.php
+++ page.php
@@ -2,14 +2,9 @@
 /**
  * The template for displaying all pages
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package idealist
+ * @package Idealist
  */
 
 get_header(); ?>
