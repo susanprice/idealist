91bd679579b818423ca32ec7314c280b4a7ce742
Susan Price <susaneprice@gmail.com>
2017-04-04 17:48:57 -0700

    fixed comments for translators

--- search.php
+++ search.php
@@ -4,15 +4,15 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
  *
- * @package idealist
+ * @package Idealist
  */
 
 get_header(); ?>
 
 	<section class="img-responive" data-type="background" data-speed="2">
-		/* translators: displays search term entered */
+		<!--  translators: displays search term entered -->
 		<h1 class="page-title">
-		/* translators: displays search term entered */		
+		<!-- translators: displays search term entered -->		
 		<?php printf( esc_html__( 'Search Results for: %s', 'idealist' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 	</section>	
 
@@ -25,22 +25,13 @@ get_header(); ?>
 					<?php
 					/* Start the Loop */
 					while ( have_posts() ) : the_post();
-
-						/**
-						 * Run the loop for the search to output the results.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-search.php and that will be used instead.
-						 */
 						get_template_part( 'template-parts/content', 'search' );
-
 					endwhile;
 
 					the_posts_navigation();
 
 				else :
-
 					get_template_part( 'template-parts/content', 'none' );
-
 				endif; ?>
 			</main><!-- #content -->
 			<!-- SIDEBAR
