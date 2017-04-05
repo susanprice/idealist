a36dcdf4c43fa1f2f8dacf3979764facf95741ca
Susan Price <susaneprice@gmail.com>
2017-04-04 17:37:48 -0700

    version 1.0.0 submitted

--- category.php
+++ category.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package idealist
+ * @package Idealist
  */
 
 get_header(); ?>
@@ -41,14 +41,7 @@ if ( have_posts() ) : ?>
 				<?php
 				/* Start the Loop */
 				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
 					get_template_part( 'template-parts/content', get_post_format() );
-
 				endwhile;
 
 				the_posts_navigation();
