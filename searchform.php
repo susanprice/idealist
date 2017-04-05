a0102f4905142dd3cce1c2c74c164694ae383c70
Susan Price <susaneprice@gmail.com>
2017-04-04 17:51:06 -0700

    added escaping functions

--- searchform.php
+++ searchform.php
@@ -7,9 +7,9 @@
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="<?php echo $unique_id; ?>">
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'idealist' ); ?></span>
+	<label for="<?php echo esc_attr( $unique_id ); ?>">
+		<span class="screen-reader-text"><?php echo esc_attr( 'Search for:', 'label', 'idealist' ); ?></span>
 	</label>
-	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'idealist' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" class="search-submit"><?php echo idealist_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'idealist' ); ?></span></button>
+	<input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'idealist' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	<button type="submit" class="search-submit"><?php echo idealist_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php echo esc_attr( 'Search', 'submit button', 'idealist' ); ?></span></button>
 </form>
