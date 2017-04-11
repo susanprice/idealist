4675472779221ecff138e77b842f16308177ead4
Susan Price <susaneprice@gmail.com>
2017-04-10 18:39:07 -0700

    display string in footer entered in customizer

--- footer.php
+++ footer.php
@@ -11,31 +11,39 @@
 
 ?>
 
+
+<?php 
+    $custom_footer_id = get_theme_mod( 'copyright_id' );
+?>
+
+
 <!-- Footer
 ================================================== -->
-<div class="navbar-wrapper">
-    <nav class="navbar">
-        <div class="container-fluid">
-        <hr>
-            <div class="col-sm-12 col-md-12 col-lg-12">
-                <nav>
-                    <!-- ?php wp_nav_menu( array( 
-                        'menu'              => 'social',
-                        'theme_location'    => 'social',
-                        'container_class'   => 'social_menu_class',
-                        'link_before'       => '<span class="screen-reader-text">',
-                        'link_after'        => '</span>',
-                        'fallback_cb'       => false
-                         ) ); ? -->  
-                </nav>
-            </div>    
+<footer>
+    <div class="navbar-wrapper">
+        <nav class="navbar">
+            <div class="container-fluid">
+            <hr>
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <nav>
+                        <!-- ?php wp_nav_menu( array( 
+                            'menu'              => 'social',
+                            'theme_location'    => 'social',
+                            'container_class'   => 'social_menu_class',
+                            'link_before'       => '<span class="screen-reader-text">',
+                            'link_after'        => '</span>',
+                            'fallback_cb'       => false
+                             ) ); ? -->  
+                    </nav>
+                </div>    
 
-            <div class="col-sm-12 col-md-12 col-lg-12">
-                <!-- p class="copyright">Copyright &copy; 2017</p -->
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <p class="copyright"><?php echo esc_attr( $custom_footer_id ); ?></p>
+                </div>
             </div>
-        </div>
-    </nav>
-</div>
+        </nav>
+    </div>
+</footer>    
 
 
 <?php wp_footer(); ?>
