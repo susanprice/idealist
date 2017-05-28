aa63b8480993bdc4b84d56b097c6a8a4f9a04362
Susan Price <susaneprice@gmail.com>
2017-05-27 18:22:05 -0700

    Split into two separate hooks: idealist_customize_register() & idealist_customizer(). New top level panel ‘Idealist Options’ containing ‘Main Options’ section (2 new radio buttons) and new ‘Documentation’ section. Removed content text color and content link color code

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,64 +14,86 @@
  */
 
 function idealist_customize_register( $wp_customize ) {
-
-    // Colors section: add settings and controls to the existing colors section    
-    $colors = array();
-    $colors[] = array(
-        'slug'=>'content_text_color', 
-        'default' => '#333',
-        'label' => __('Content Text Color', 'idealist')
-    );
-    $colors[] = array(
-        'slug'=>'content_link_color', 
-        'default' => '#88C34B',
-        'label' => __('Content Link Color', 'idealist')
-    );
-
-    foreach( $colors as $color ) {
-        // SETTINGS
-        $wp_customize->add_setting(
-            $color['slug'], array(
-              'default' => $color['default'],
-              'type' => 'theme_mod', 
-              'capability' => 'edit_theme_options',
-              'sanitize_callback' => 'sanitize_hex_color',
-            )
-        );
-        // CONTROLS
-        $wp_customize->add_control(
-            new WP_Customize_Color_Control(
-                $wp_customize,
-                $color['slug'], 
-                array('label'   => $color['label'], 
-                    'section'   => 'colors',
-                    'settings'  => $color['slug'])
-                )
-            );
-        }
-
-   // Support selective refresh
-    $wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+    // Support selective refresh and improve responsiveness with postMessage
+    $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
     $wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
      
+    // Abort if selective refresh is not available
+    if ( ! isset( $wp_customize->selective_refresh ) ) {
+        return;
+    }
+
+    // Support selective refresh for Idealist Options
     $wp_customize->selective_refresh->add_partial( 'blogname', array(
         'selector' => '.site-title',
         'render_callback' => 'idealist_customize_partial_blogname',
     ) );
+
     $wp_customize->selective_refresh->add_partial( 'blogdescription', array(
         'selector' => '.site-description',
         'render_callback' => 'idealist_customize_partial_blogdescription',
     ) );
+
     $wp_customize->selective_refresh->add_partial( 'logo', array(
         'selector' => 'img.custom-logo',
         'render_callback' => 'idealist_customize_partial_logo',
     ) );
 
+    $wp_customize->selective_refresh->add_partial( 'copyright_id', array(
+        'selector' => '#copyright',
+        'render_callback' => 'idealist_customize_partial_copyright',
+    ) );
+}
 
-    // Add Theme Options section
+add_action( 'customize_register', 'idealist_customize_register' );
+
+
+/**
+ * Options for WordPress Theme Customizer.
+ */
+function idealist_customizer( $wp_customize ) {
+ 
+    // Idealist Options Panel 
+    $wp_customize->add_panel( 'idealist_main_options', array(
+        'capability'     => 'edit_theme_options',
+        'theme_supports' => '',
+        'title'          => esc_html__( 'Idealist Options', 'idealist' ),
+        'description'    => esc_html__( 'Panel to update Idealist theme options', 'idealist' ), // in case of html tags such as <p>
+        'priority'       => 10, 
+    ) );
+
+    // Idealist Options Sections
     $wp_customize->add_section( 'theme_options' , array(
-        'title'      => __( 'Idealist Options', 'idealist' ),
-        'priority'   => 130,
+        'title'      => __( 'Main Options', 'idealist' ),
+        'priority'   => 20,
+        'panel'       => 'idealist_main_options',
+    ) );
+
+    $wp_customize->add_section( 'theme_support' , array(
+        'title'      => __( 'Documentation', 'idealist' ),
+        'priority'   => 30,
+        'panel'       => 'idealist_main_options',
+    ) );
+
+    // Idealist 'Main Options' Settings & Controls
+    $wp_customize->add_setting( 'show_borders_id', array(
+        'default'    => '1',
+    ) );
+
+    $wp_customize->add_control( 'show_borders_id', array(
+        'label'      => __( 'Show Borders', 'documentation' ),
+        'section'    => 'theme_options',
+        'type'       => 'checkbox',
+    ) );
+
+    $wp_customize->add_setting( 'show_comments_badge_id', array(
+        'default'    => '1',
+    ) );
+
+    $wp_customize->add_control( 'show_comments_badge_id', array(
+        'label'      => __( 'Show Comments Badge', 'documentation' ),
+        'section'    => 'theme_options',
+        'type'       => 'checkbox',
     ) );
 
     $wp_customize->add_setting( 'copyright_id', array(
@@ -92,13 +114,27 @@ function idealist_customize_register( $wp_customize ) {
         'input_attrs'          => array(
         'style'                => 'border: 1px solid #ccc',
         'placeholder'          => __( 'Enter text here to display in footer.', 'idealist' ),
-         ),
+        ),
     ) );
 
-   // Support selective refresh for Theme Options
-    $wp_customize->selective_refresh->add_partial( 'copyright_id', array(
-    'selector' => '.copyright',
-    'render_callback' => 'idealist_customize_partial_copyright',
+    // Idealist 'Documentation/Support' Settings & Controls
+    $wp_customize->add_setting( 'support_id', array(
+    ) );
+
+    $wp_customize->add_setting( 'thanks_id', array(
+    ) );
+
+    $wp_customize->add_control( 'support_id', array(
+        'label'                => __( '', 'idealist' ),
+        'type'                 => 'hidden',
+        'section'              => 'theme_support',      
+        'description'          => esc_html__( 'Find the FAQ and documentation here: https://wpvisuals.com/support/idealist', 'idealist' ),
+    ) );
+
+    $wp_customize->add_control( 'thanks_id', array(
+        'label'                => __( 'Thanks for using Idealist!', 'idealist' ),
+        'type'                 => 'hidden',
+        'section'              => 'theme_support',      
     ) );
 
     // Hide core sections/controls when they aren't used on the current page.
@@ -106,8 +142,14 @@ function idealist_customize_register( $wp_customize ) {
     $wp_customize->get_control( 'blogdescription' )->active_callback = 'is_front_page';
 }
 
-add_action( 'customize_register', 'idealist_customize_register' );
+add_action( 'customize_register', 'idealist_customizer' );
 
+/**
+ * Sanitize a URL 
+ */
+function idealist_sanitize_url( $url ) {
+    return esc_url_raw( $url );
+}
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
