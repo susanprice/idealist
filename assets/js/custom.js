cd6db9383f92ded213a99f75f955020f8b0ff680
Susan Price <susaneprice@gmail.com>
2017-05-27 18:23:29 -0700

    added comments

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,12 @@
 /**
- * 
+ * utility UI functions
  * custom.js
- * 
- *
  */
 
-// window.alert('hello');
+// window.alert('custom.js');
 
 function showSearchInput() {
     document.getElementById("search-entry").innerHTML = "in showSearchInput()";
     document.getElementById("search-entry").style.visibility = "visible";
     document.getElementById("main-navbar-collapse-1").style.visibility = "hidden";
-
 }
