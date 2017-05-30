0bcf3dbbc59b7e698f3acc11eeb2e18985b4c54a
Susan Price <susaneprice@gmail.com>
2017-05-30 14:22:28 -0700

    removed unused code for social menu

--- footer.php
+++ footer.php
@@ -24,19 +24,6 @@
         <nav class="navbar">
             <div class="container-fluid">
                 <div class="col-sm-12 col-md-12 col-lg-12">
-                    <nav>
-                        <!-- ?php wp_nav_menu( array( 
-                            'menu'              => 'social',
-                            'theme_location'    => 'social',
-                            'container_class'   => 'social_menu_class',
-                            'link_before'       => '<span class="screen-reader-text">',
-                            'link_after'        => '</span>',
-                            'fallback_cb'       => false
-                             ) ); ? -->  
-                    </nav>
-                </div>    
-
-                <div class="col-sm-12 col-md-12 col-lg-12">
                     <p class="copyright"><?php echo esc_attr( $idealist_footer_copyright ); ?></p>
                     <p class="credits"><?php echo esc_html("WordPress Theme Idealist"); ?>
                 </div>
