22cd6fc0e1e9473c7f07c8e131d0d030e667ac08
Susan Price <susaneprice@gmail.com>
2017-06-26 12:51:20 -0700

    initial commit

---
+++ template-parts/page-two-panel.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template Name: Two Panel Template
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Idealist
+ */
+
+get_header(); ?>
+
+	<div class="container">
+		<div class="row" id="primary">
+        
+			<!-- MAIN CONTENT
+            ================================================== -->	
+			<main id="content" class="col-sm-8" role="main">
+			<?php
+				if ( have_posts() ) :
+
+					if ( is_home() && ! is_front_page() ) : ?>
+						<header>
+							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+						</header>
+
+					<?php
+					endif;
+
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+						get_template_part( 'template-parts/content', 'page' );
+					endwhile; // End of the loop.
+				endif;	
+				?>
+			</main><!-- #main -->
+
+			<!-- SIDEBAR
+			================================================== -->
+			<aside class="col-sm-4">
+			<!-- this is defined in Appearance > Widgets -->
+			<!-- ?php get_sidebar( 'primary' ); ? -->
+
+			<!-- this is a minimal, default sidebar if a sidebar is not defined in Appearance > Widgets -->
+			<?php get_sidebar( ); ?>
+
+			</aside>
+
+		</div><!-- #primary -->
+	</div>	
+<?php
+get_footer();
