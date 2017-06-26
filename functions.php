e7dd9b0263bbed6315f5b3cd9a5654ad57428845
Susan Price <susaneprice@gmail.com>
2017-06-26 08:15:42 -0700

    enqueue custom.js

--- functions.php
+++ functions.php
@@ -194,6 +194,8 @@ function idealist_scripts() {
 
 	wp_enqueue_script( 'idealist-skip-link-focus-fix', get_theme_file_uri( '/assets/js/skip-link-focus-fix.js' ), array(), '1.0', true );
 
+	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/js/custom.js', array(), '20160804', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
