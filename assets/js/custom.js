683f9f473fd683c70394f9467755f9ad4f546a02
Susan Price <susaneprice@gmail.com>
2017-06-28 07:17:11 -0700

    removed test and commented out code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,9 +5,6 @@
 
 
 function showSearchInput() {
-    // window.alert('custom.js');
-    // document.getElementById("full-search").innerHTML = "in showSearchInput()";
     document.getElementById("full-search").style.visibility = "visible";
     document.getElementById("full-search").style.display = "block";
-    // document.getElementById("main-navbar-collapse-1").style.visibility = "hidden";
 }
