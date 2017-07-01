69fae1c5f1f51c3f8a68d14ab65e96d4169b6205
Susan Price <susaneprice@gmail.com>
2017-06-30 22:04:53 -0700

    search in nav bar: set focus in entry field and reset text on close/reset

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,8 +7,11 @@
 function showSearchInput() {
     document.getElementById("full-search").style.visibility = "visible";
     document.getElementById("full-search").style.display = "block";
+    document.getElementById('search').focus();
 }
 
+// user hit the close/reset button
 function hideSearchInput() {
     document.getElementById("full-search").style.display = "none";
+    document.forms["full-search"].reset();
 }
