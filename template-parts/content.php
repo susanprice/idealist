bff134aeedf9919c8d4ffaa17c50bffc32e5133f
Susan Price <susaneprice@gmail.com>
2017-07-01 18:35:40 -0700

    removed debug code

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,30 +8,6 @@
  */
 ?>
 
-<!-- ?php $badgedefault = get_theme_mod( 'show_comments_badge_id' );
-echo "badge is set to: [";
-echo $badgedefault;
-echo "]" . "<br>";
-
-if ( get_theme_mod( 'show_comments_badge_id' ) !== FALSE ) {
-	echo "value is not FALSE" . "<br>";
-}
-
-if ( get_theme_mod( 'show_comments_badge_id' ) !== TRUE ) {
-	echo "value is not TRUE" . "<br>";
-}
-
-if ( get_theme_mod( 'show_comments_badge_id' ) === FALSE ) {
-	echo "value is FALSE" . "<br>";
-}
-
-echo "var_dump of badgedefault: ";
-var_dump( $badgedefault );
-echo "<br>";
-echo "<br>";
-
-? -->
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title post-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
