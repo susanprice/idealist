f89a4235399992d6fa3819b6b9eb31e75c6f6c2b
Susan Price <susaneprice@gmail.com>
2017-07-08 11:13:46 -0700

    removed unused extras.php & jetpack.php

--- functions.php
+++ functions.php
@@ -210,14 +210,8 @@ require get_template_directory() . '/inc/custom-header.php';
 // Custom template tags for this theme
 require get_template_directory() . '/inc/template-tags.php';
 
-// Custom functions that act independently of the theme templates
-require get_template_directory() . '/inc/extras.php';
-
 // Customizer additions
 require get_template_directory() . '/inc/customizer.php';
-
-// Load Jetpack compatibility file
-require get_template_directory() . '/inc/jetpack.php';
 
 // Register custom navigation walker
 require_once get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
