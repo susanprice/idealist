a891e2edcbcb0276eed5255f4b7e8b87b7cabc4b
Susan Price <susaneprice@gmail.com>
2017-07-10 08:02:20 -0700

    updated widgets description to explain when it is displayed.

--- functions.php
+++ functions.php
@@ -114,7 +114,7 @@ function idealist_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Primary Sidebar', 'idealist' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here to display on the right side of your posts. For example, use the Recent Posts and Recent Comments widgets to engage your readers. Or, use the Text widget to display custom info and special announcments.', 'idealist' ),
+		'description'   => esc_html__( 'Add widgets here to display on the right side of your posts. This sidebar is displayed on the Front Page by default, and with the Two Panel Template.', 'idealist' ),
 		'class'			=> 'minimal',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
