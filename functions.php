732aec5fd8d6c8892bf7119e874a2472b8c652fe
Susan Price <susaneprice@gmail.com>
2017-07-10 07:22:08 -0700

    added to the widget description, including examples

--- functions.php
+++ functions.php
@@ -114,7 +114,7 @@ function idealist_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Primary Sidebar', 'idealist' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'idealist' ),
+		'description'   => esc_html__( 'Add widgets here to display on the right side of your posts. For example, use the Recent Posts and Recent Comments widgets to engage your readers. Or, use the Text widget to display custom info and special announcments.', 'idealist' ),
 		'class'			=> 'minimal',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
