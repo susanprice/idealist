c854581465082496a35073fa59a93755a4caecfb
Susan Price <susaneprice@gmail.com>
2017-08-28 12:19:58 -0700

    swapping in new pagination

--- archive.php
+++ archive.php
@@ -32,7 +32,7 @@ get_header(); ?>
 							get_template_part( 'template-parts/content', get_post_format() );
 						endwhile;
 
-						the_posts_navigation();
+						get_template_part( 'template-parts/page/pagination' );
 
 						else :
 
