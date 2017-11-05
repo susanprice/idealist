19392bac04c9a717014d27e32293a9a239dad38c
Susan Price <susaneprice@gmail.com>
2017-11-05 14:28:30 -0800

    added link to NewMediaThemes.com in footer

--- footer.php
+++ footer.php
@@ -25,7 +25,14 @@
             <div class="container-fluid">
                 <div class="col-sm-12 col-md-12 col-lg-12">
                     <p class="copyright"><?php echo esc_html( $idealist_footer_copyright ); ?></p>
-                    <p class="credits"><?php echo esc_html("WordPress Theme: Idealist"); ?>
+                    <p class="credits">
+                        <?php printf(
+                            '%1$s <a href="%2$s">%3$s</a>',
+                            esc_html__( 'Idealist by', 'idealist' ),
+                            esc_attr( 'https://NewMediaThemes.com/' ),
+                            esc_html( 'NewMediaThemes' )
+                        ); ?>
+                    </p>    
                 </div>
             </div>
         </nav>
