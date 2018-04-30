7849c48cbf6ded7a5572ea1ec2683210b11e4822
Susan Price <susaneprice@gmail.com>
2018-04-29 21:03:51 -0700

    About Idealist page including helper functions to define allowed html

---
+++ inc/admin/about-page.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * About Idealist Page
+ *
+ * @since Idealist 1.1.5
+ *
+ */
+function idealist_welcome_page() { 
+    $html = '<div class="about">';
+        $html .= '<br><h1 class="welcome">Welcome to Idealist!</h1><br>';
+
+        $html .= '<h3 class="sub-heading">Theme Options & Quick Start Guide</h3>';
+
+        $html .= '<p class="description">Use the WordPress Customizer to set theme options and view the Quick Start Guide.</p>';
+                
+        $html .= '<a class="button button-primary" target="_blank" href="customize.php">Go to Customizer</a><br>'; 
+
+        $html .= '<br>';
+
+        $html .= '<h3>Questions?</h3>';
+
+        $html .= '<p class="description">If you have questions about the Idealist theme, please post your questions in the community forum.</p>';
+
+        $html .= '<a class="button button-primary" target="_blank" href="https://wordpress.org/support/theme/idealist">Go to Support Forum</a><br>';
+
+        $html .= '<br>';
+
+        $html .= '<h3>Subscribe to Design News</h3>';
+
+        $html .= '<p class="description">If you would like to receive design news, sign up here:</p>';
+
+        $html .= '<a class="button button-primary" target="_blank" href="https://NewMediaThemes.com">NewMediaThemes.com</a><br>';  
+        
+        $html .= '<br><p class="description">Enjoying Idealist? Please consider leaving a five star review on WordPress.org. We\'d really appreciate it!</p>'; 
+
+        $html .= '</div>';
+  
+    echo wp_kses( $html, shapeSpace_allowed_html() );   
+}
+?>
---
+++ inc/helper-functions.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *
+ * Idealist: Helper functions
+ *
+ * @package Idealist
+ *
+ */
+
+/**
+ * Defines an array of allowed html to pass to kses functions
+ *
+ * https://wp-mix.com/wordpress-basic-allowed-html-wp_kses/
+ */
+
+function shapeSpace_allowed_html() {
+
+    $allowed_tags = array(
+        'a' => array(
+            'class' => array(),
+            'href'  => array(),
+            'rel'   => array(),
+            'title' => array(),
+        ),
+        'abbr' => array(
+            'title' => array(),
+        ),
+        'b' => array(),
+        'blockquote' => array(
+            'cite'  => array(),
+        ),
+        'br' => array(),        
+        'cite' => array(
+            'title' => array(),
+        ),
+        'code' => array(),
+        'del' => array(
+            'datetime' => array(),
+            'title' => array(),
+        ),
+        'dd' => array(),
+        'div' => array(
+            'class' => array(),
+            'title' => array(),
+            'style' => array(),
+        ),
+        'dl' => array(),
+        'dt' => array(),
+        'em' => array(),
+        'h1' => array(),
+        'h2' => array(),
+        'h3' => array(),
+        'h4' => array(),
+        'h5' => array(),
+        'h6' => array(),
+        'i' => array(),
+        'img' => array(
+            'alt'    => array(),
+            'class'  => array(),
+            'height' => array(),
+            'src'    => array(),
+            'width'  => array(),
+        ),
+        'li' => array(
+            'class' => array(),
+        ),
+        'ol' => array(
+            'class' => array(),
+        ),
+        'p' => array(
+            'class' => array(),
+        ),
+        'q' => array(
+            'cite' => array(),
+            'title' => array(),
+        ),
+        'span' => array(
+            'class' => array(),
+            'title' => array(),
+            'style' => array(),
+        ),
+        'strike' => array(),
+        'strong' => array(),
+        'ul' => array(
+            'class' => array(),
+        ),
+    );
+    
+    return $allowed_tags;
+}
